9b6981aa6211d8f528469c87052cfdbf0b290f94
Smit Gabani <63806909+smitgabani@users.noreply.github.com>
2020-10-14 00:16:19 -0400

    Create quick-sort.py

---
+++ sorting/quick-sort.py
@@ -0,0 +1,20 @@
+def partition(A, p, r):
+    x = A[r]
+    i = (p - 1)
+    for j in range(p, r):
+        if A[j] < x:
+            i = i + 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return (i+1)
+
+def quickSort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quickSort(A, p, q-1)
+        quickSort(A, q+1, r)
+
+A = [9,8,7,6,5,4,3,2,1]
+n = len(A)
+quickSort(A, 0, n-1)
+print(A)
